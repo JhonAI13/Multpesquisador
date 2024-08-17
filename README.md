5ac42927cab91a19b1e63e874d607bb4a6fee489
Jonathas Martins da Rocha <84166332+JhonAI13@users.noreply.github.com>
2024-08-17 01:47:41 -0300

    att

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Multpesquisador
  Será um site onde pesquisara e abrirar abas no navegador de varios sites, estes escolhidos antes da pesquisa.
+# Coisas a implementar
+- [ ] 5 sites no mínimo
+- [ ] Um sistema para escolher os sites.
+- [ ] Conseguir rodar no terminal
--- programa/app.py
+++ programa/app.py
@@ -1,16 +1,51 @@
 import webbrowser
-iwebsites = ['https://www.google.com/search?q=', 
-            'https://pt.wikipedia.org/wiki/']
+iwebsites = {"google":'https://www.google.com/search?q=', 
+            "wikipedia":'https://pt.wikipedia.org/wiki/'}
 
-def pesquise(websites,pesquisa):
+
+
+def interface():
+    lista = []
+    resposta = str(input('Quer mudar os sites')).strip()
+    if resposta in ("si", "sim", "s"):
+        with open(r'programa\sites.txt', 'r') as arquivo:
+            n = 0
+            for linha in arquivo:
+                if n != 0: #Verifica se é a primeira linha
+                    if n % 2 == 0: #Anda nas linhas pares - a 1 linha
+                        print(linha)
+                        lista.append(str(linha))
+                n += 1
+    else:
+        with open(r'programa\sites.txt', 'r') as arquivo:
+            n = 0
+            for linha in arquivo:
+                if n != 0: 
+                    if n % 2 != 0: 
+                        print(f'[linha]', end='')
+                n += 1
+        # perguntar quais os sites o usuario quer
+            # google, wiki, google escola, translate
+            # (1,2 , 3)
+            # salvar na lista quais sites foram escolhidos
+            # vai colocar os sites na lista em str
+         
+    return lista
+
+
+def pesquise(websites, pesquisa): # mudar para funcionar com lsita
     """abre abas pesquisando, a quantidade de sites da lista.
 
     Args:
         websites (lista): _description_
         pesquisa (string): _description_
     """
-    for url in websites:
-        webbrowser.open(url + pesquisa)
+    for url in websites: 
+        webbrowser.open(websites[url] + pesquisa)
 
-pesquisa = str(input("Mulltipesquisador \n"))
-pesquise(iwebsites, pesquisa)
+
+def pergunte():
+    escolhidos = interface()
+    return escolhidos, str(input("Mulltipesquisador \n"))
+    
+pesquise(iwebsites, pergunte())
